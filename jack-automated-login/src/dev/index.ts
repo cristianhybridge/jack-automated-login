8f7222f2ba33b4ab2a1666843cd3f5d6c5b81086
Cristian Mendoza <FC-CEMH@outlook.com>
2025-12-15 01:22:04 -0800

    added readme.md

--- jack-automated-login/src/dev/index.ts
+++
@@ -1,6 +0,0 @@
-﻿import React from "react";
-import { useInitial } from "./useInitial";
-
-const ComponentPreviews = React.lazy(() => import("./previews"));
-
-export { ComponentPreviews, useInitial };
